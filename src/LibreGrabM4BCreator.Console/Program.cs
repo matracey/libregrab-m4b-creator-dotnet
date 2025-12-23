f5f9c56d9903f1d6892407c9648605c4a6f5f781
Martin Tracey <matracey@outlook.ie>
2025-12-23 20:12:45 +0000

    chore(console): add sample code for ConsoleAppFramework and FFMpegCore.

--- src/LibreGrabM4BCreator.Console/Program.cs
+++ src/LibreGrabM4BCreator.Console/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using ConsoleAppFramework;
+
+using Spectre.Console;
+
+using FFMpegCore;
+
+// Use ConsoleAppFramework for argument parsing
+var app = ConsoleApp.Create();
+
+app.Add("transcode", async (string input, string output) =>
+{
+    await AnsiConsole.Progress()
+        .StartAsync(async ctx =>
+        {
+            var task = ctx.AddTask("[green]Transcoding...[/]");
+
+            // Example FFMpegCore usage
+            var mediaInfo = await FFProbe.AnalyseAsync(input);
+            var totalDuration = mediaInfo.Duration;
+
+            await FFMpegArguments
+                .FromFileInput(input)
+                .OutputToFile(output, true, options => options
+                    .WithVideoCodec("libx264")
+                    .WithAudioCodec("aac"))
+                .NotifyOnProgress(time => task.Value = time.TotalSeconds / totalDuration.TotalSeconds * 100)
+                .ProcessAsynchronously();
+        });
+
+    AnsiConsole.MarkupLine("[bold blue]Done![/]");
+});
+
+app.Run(args);
